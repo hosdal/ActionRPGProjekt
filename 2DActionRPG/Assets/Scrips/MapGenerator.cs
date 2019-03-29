759f44d61682aa032458cb2e635530560e048c2e
Bonde <chrbonde@live.dk>
2019-03-29 11:51:59 +0100

    Rooms script

--- 2DActionRPG/Assets/Scrips/MapGenerator.cs
+++ 2DActionRPG/Assets/Scrips/MapGenerator.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapGenerator : MonoBehaviour
-{
+public class MapGenerator : MonoBehaviour {
     public GameObject rooms;
     public int maxroom;
     private int counter = 0;
     private Object[] roomPrefabs;
+    private List<Vector2> positions = new List<Vector2>();
 
     void Awake()
     {
@@ -16,15 +16,16 @@ public class MapGenerator : MonoBehaviour
             roomPrefabs = Resources.LoadAll("Rooms");
             var roomslist = rooms.GetComponentsInChildren<Room>();
             GameObject room = roomslist[Random.Range(0, roomslist.Length)].gameObject;
-            Debug.Log(room);
             Room script = room.GetComponentInChildren<Room>();
             int index = Random.Range(0, script.has_doors.Length);
+            Vector2 pos = new Vector2();
             if (script.has_doors[index])
             {
                 switch (index)
                 {
                     case 0:
-                        if (script.left_room == null)
+                        pos = new Vector2(room.transform.position.x - 13.5f, room.transform.position.y);
+                        if (script.left_room == null && !isPosInArray(pos, positions))
                         {
                             GameObject obj = null;
                             bool found = false;
@@ -42,11 +43,13 @@ public class MapGenerator : MonoBehaviour
                             newobj.transform.parent = rooms.transform;
                             script.left_room = newobj;
                             newscript.right_room = room.transform.gameObject;
-                            newobj.transform.position = new Vector2(room.transform.position.x - 13.5f, room.transform.position.y);
+                            newobj.transform.position = pos;
+                            positions.Add(pos);
                         }
                         break;
                     case 1:
-                        if (script.right_room == null)
+                        pos = new Vector2(room.transform.position.x + 13.5f, room.transform.position.y);
+                        if (script.right_room == null && !isPosInArray(pos, positions))
                         {
                             GameObject obj = null;
                             bool found = false;
@@ -64,11 +67,13 @@ public class MapGenerator : MonoBehaviour
                             newobj.transform.parent = rooms.transform;
                             script.right_room = newobj;
                             newscript.left_room = room.transform.gameObject;
-                            newobj.transform.position = new Vector2(room.transform.position.x + 13.5f, room.transform.position.y);
+                            newobj.transform.position = pos;
+                            positions.Add(pos);
                         }
                         break;
                     case 2:
-                        if (script.up_room == null)
+                        pos = new Vector2(room.transform.position.x, room.transform.position.y +10f);
+                        if (script.up_room == null && !isPosInArray(pos, positions))
                         {
                             GameObject obj = null;
                             bool found = false;
@@ -86,11 +91,13 @@ public class MapGenerator : MonoBehaviour
                             newobj.transform.parent = rooms.transform;
                             script.up_room = newobj;
                             newscript.down_room = room.transform.gameObject;
-                            newobj.transform.position = new Vector2(room.transform.position.x, room.transform.position.y + 10f);
+                            newobj.transform.position =pos;
+                            positions.Add(pos);
                         }
                         break;
                     case 3:
-                        if (script.down_room == null)
+                        pos = new Vector2(room.transform.position.x, room.transform.position.y-10f);
+                        if (script.down_room == null && !isPosInArray(pos, positions))
                         {
                             GameObject obj = null;
                             bool found = false;
@@ -108,13 +115,24 @@ public class MapGenerator : MonoBehaviour
                             newobj.transform.parent = rooms.transform;
                             script.down_room = newobj;
                             newscript.up_room = room.transform.gameObject;
-                            newobj.transform.position = new Vector2(room.transform.position.x, room.transform.position.y - 10f);
+                            newobj.transform.position = pos;
+                            positions.Add(pos);
                         }
                         break;
                 }
             }
 
         }
+    }
+
+    bool isPosInArray(Vector2 val, List<Vector2> arr)
+    {
+        for (int i = 0; i < arr.Count; i++)
+        {
+            if (arr[i] == val)
+                return true;
+        }
+        return false;
     }
 
     void Update()
