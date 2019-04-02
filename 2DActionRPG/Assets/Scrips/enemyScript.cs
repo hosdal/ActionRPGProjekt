a959714e21ae76c3b91410035a9a0b1c6d26a30c
Simon <simon051295@gmail.com>
2019-04-02 15:01:48 +0200

    saved

--- 2DActionRPG/Assets/Scrips/enemyScript.cs
+++ 2DActionRPG/Assets/Scrips/enemyScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyScript : MonoBehaviour, IDamageable
+public class enemyScript : MonoBehaviour
 {
     public Roomfloor room;
     [SerializeField]
@@ -14,33 +14,33 @@ public class enemyScript : MonoBehaviour, IDamageable
     private float speed;
 
     private PlayerScript player;
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<PlayerScript>();
-        currentHp = maxHp;
     }
 
     private void FixedUpdate()
     {
+
         if (room.isIn)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.transform.position - transform.position), speed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), new Vector2(player.transform.position.x, player.transform.position.y), speed * Time.deltaTime);
+
+            Vector3 diff = (player.transform.position - transform.position);
+
+
+
+            float angle = Mathf.Atan2(diff.y, diff.x);
+            transform.rotation = Quaternion.Euler(0f, 0f, angle * Mathf.Rad2Deg);
+
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.transform.position - transform.position), speed * Time.deltaTime);
 
             //transform.position += transform.forward * speed * Time.deltaTime;
 
         }
     }
-
-
-    public void AddDamage(int damage)
-    {
-        currentHp -= damage;
-        if (currentHp == 0)
-        {
-            Destroy(gameObject);
-        }
-    }
 }
+    
