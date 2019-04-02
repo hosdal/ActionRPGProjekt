fd4d2dbb5352a033e6ded5ead92c6ad271afd9c8
Leaq <frederikgrummesgaard@gmail.com>
2019-04-02 12:49:47 +0200

    Shoot animatition & Enemy fields

--- 2DActionRPG/Assets/Scrips/enemyScript.cs
+++ 2DActionRPG/Assets/Scrips/enemyScript.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class enemyScript : MonoBehaviour
 {
+    public Roomfloor room;
+    [SerializeField]
+    private int maxHp;
+    private int currentHp;
+    [SerializeField]
+    private int attackPower;
+    [SerializeField]
+    private float speed;
+    
+
     // Start is called before the first frame update
     void Start()
     {
