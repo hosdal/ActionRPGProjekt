aa5d9be7491886287048097f1733cbef45ab0b0e
Leaq <frederikgrummesgaard@gmail.com>
2019-04-02 12:12:02 +0200

    Bullets virker endeligt

--- 2DActionRPG/Assets/Scrips/bulletScript.cs
+++ 2DActionRPG/Assets/Scrips/bulletScript.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class bulletScript : MonoBehaviour
 {
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.gameObject.name);
-     if(collision.gameObject.tag == "Enemy")
+        //Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.tag == "Enemy")
         {
-            Debug.Log("hit");
-        } else if(collision.gameObject.tag == "Wall" || collision.gameObject.tag == "Door")
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.tag == "Wall" || collision.gameObject.tag == "Door")
         {
             Destroy(gameObject);
         }
