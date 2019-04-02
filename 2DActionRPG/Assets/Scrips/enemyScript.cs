9f79af71a4ca0299e2a9d2de7369349cda33873e
Bonde <chrbonde@live.dk>
2019-04-02 14:57:20 +0200

    prepare

--- 2DActionRPG/Assets/Scrips/enemyScript.cs
+++ 2DActionRPG/Assets/Scrips/enemyScript.cs
@@ -14,7 +14,7 @@ public class enemyScript : MonoBehaviour, IDamageable
     private float speed;
 
     private PlayerScript player;
-
+    
 
     // Start is called before the first frame update
     void Start()
@@ -24,25 +24,16 @@ public class enemyScript : MonoBehaviour, IDamageable
     }
 
     private void FixedUpdate()
-    { 
-       
+    {
         if (room.isIn)
         {
-            transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), new Vector2(player.transform.position.x, player.transform.position.y), speed*Time.deltaTime);
-
-            Vector3 diff = (player.transform.position - transform.position);
-
-   
-
-            float angle = Mathf.Atan2(diff.y, diff.x);
-            transform.rotation = Quaternion.Euler(0f, 0f, angle * Mathf.Rad2Deg);
-          
-            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.transform.position - transform.position), speed * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.transform.position - transform.position), speed * Time.deltaTime);
 
             //transform.position += transform.forward * speed * Time.deltaTime;
 
         }
     }
+
 
     public void AddDamage(int damage)
     {
