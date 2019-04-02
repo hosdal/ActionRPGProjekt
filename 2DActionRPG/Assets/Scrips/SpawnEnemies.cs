89518612e05446eb9edbf87584504c9e34e2146b
Bonde <chrbonde@live.dk>
2019-04-02 11:33:01 +0200

    spawnenemies

---
+++ 2DActionRPG/Assets/Scrips/SpawnEnemies.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    private bool firstenter = false;
+    public Rigidbody2D enemyPrefab;
+    public List<GameObject> enemeyspawns;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            firstenter = true;
+        }
+    }
+}
